2c67e5e86c784196cdcc7f87f280a2ad017e1711
Teaboyd <ph.patiphan@kkumail.com>
2024-03-22 01:47:29 +0700

    JavaScript Values Variables Assignment

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,14 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let name = "Karter";
+let surname = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const ResName = "Chef's Table by Chef James";
+const TotalBill = 15000;
+const TotalRes = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+TotalBill = 5000;
+TotalRes = 2500;
